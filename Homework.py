7521f6eefed6964e3f8e3207b2bb06fc8b812349
Revitalb10 <revitalb10@gmail.com>
2019-07-19 07:36:00 +0300

    Initial commit

---
+++ Homework.py
@@ -0,0 +1,31 @@
+a = 2
+b = "bla"
+r = 4
+print (b)
+print(a)
+print(str(a)+b)
+print(a,b)
+# #print(a/r)
+
+print("the number is" ,2*3)
+#ctrld
+# ctrl/
+
+if r > a:
+    print("yes")
+if r < a:
+    print("no")
+
+
+if(r!=a and a==8):
+    print("yes")
+    if r>2:
+        print("YEHHHH")
+elif a==5:
+    print("no")
+elif r/a == 2:
+        print ("ZZZZ")
+
+
+name = input("pls...")
+print("u print:", name)

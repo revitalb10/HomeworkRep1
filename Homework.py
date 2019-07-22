4714b29327248f6d339bd41e2b33024fb6c943bf
revitalb10 <revitalb10@gmail.com>
2019-07-22 20:51:52 +0300

    Update Homework.py

--- Homework.py
+++ Homework.py
@@ -8,29 +8,3 @@ print(a,b)
 # #print(a/r)
 
 print("the number is" ,2*3)
-#ctrldhhhhhhhhhhhhzzzzzzzzzzzzzzz
-# ctrl/
-#bugfix branch change
-###1234
-#another change
-#change1
-#what is this branch
-
-if r > a:
-    print("yes.test conflict?")
-if r < a:
-    print("no")
-#what conflict?
-
-if(r!=a and a==8):
-    print("yes")
-    if r>2:
-        print("YEHHHH")
-elif a==5:
-    print("no")
-elif r/a == 2:
-        print ("ZZZZ")
-
-
-name = input("pls...")
-print("u print:", name)
